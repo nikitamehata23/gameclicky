60d4da9c4ddd4eeb26c4e0667630491d6cc0d5db
Nikita Mehata <nikitamehata23@gmail.com>
2019-07-31 10:48:57 -0500

    added the link to github repo

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,10 +4,10 @@ import './nav.css';
 const Nav = () => (
     <nav className="navbar mb-1 navigation">
       <div className="navbar-header">
-        <h2 className="navbar-brand" href="https://nikitamehata23.github.io/gameclicky/">Clicky-Clicky!!</h2>
+        <h2 className="navbar-brand" href="https://nikitamehata23.github.io/gameclicky/" target="_blank">Clicky-Clicky!!</h2>
       </div>
       <ul className="nav navbar-nav">
-        <h2 href="https://github.com/nikitamehata23/gameclicky">GitHub Repo</h2>
+        <h2 href="https://github.com/nikitamehata23/gameclicky" target="_blank">GitHub Repo</h2>
       </ul>
     </nav>
 );
