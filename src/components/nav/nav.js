5f211f89281e0394436ebfca28b772b6765b2970
Nikita Mehata <nikitamehata23@gmail.com>
2019-07-30 22:44:11 -0500

    added src folder after running in localhost

---
+++ src/components/nav/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './nav.css';
+
+const Nav = () => (
+    <nav className="navbar mb-1 navigation">
+      <div className="navbar-header">
+        <h2 className="navbar-brand" href="/">Clicky-Clicky!!</h2>
+      </div>
+      <ul className="nav navbar-nav">
+        <h2 href="https://github.com/nikitamehata23/gameclicky">GitHub Repo</h2>
+      </ul>
+    </nav>
+);
+
+export default Nav;
