89f8d5d9a899ead72eabc76bbcb2a87ee2fed746
Nikita Mehata <nikitamehata23@gmail.com>
2019-07-30 20:08:56 -0500

    Updates

---
+++ precache-manifest.ed618e8d1c07635db0435900c8cc45e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa2b73426180812e7b78a60b52a2464",
+    "url": "/gameclicky/index.html"
+  },
+  {
+    "revision": "696c3a041b38050383d8",
+    "url": "/gameclicky/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gameclicky/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "696c3a041b38050383d8",
+    "url": "/gameclicky/static/js/main.73e70c19.chunk.js"
+  },
+  {
+    "revision": "79095427ffcdb835cd45",
+    "url": "/gameclicky/static/js/runtime~main.a2d10d8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gameclicky/static/media/logo.5d5d9eef.svg"
+  }
+]);
